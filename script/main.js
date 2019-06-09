741f1b07f654f14c7eb698246713090e9fb46d7b
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-06-10 00:48:13 +0600

    add effect for article

--- script/main.js
+++ script/main.js
@@ -1,10 +1,29 @@
 let nav = document.querySelector("nav");
+let opacityObjImg = document.querySelectorAll(".opacity-img");
+let opacityObjText = document.querySelectorAll(".opacity-text");
 
 document.addEventListener("scroll", event => {
   let position = document.querySelector("body").getBoundingClientRect();
+
   if (position.y < 0) {
     nav.className = "active";
   } else {
     nav.className = "";
   }
+
+  opacityObjImg.forEach(element => {
+    let elementPos = element.getBoundingClientRect();
+
+    if (elementPos.y >= position.height / 2) {
+      element.style.opacity = "1";
+    }
+  });
+
+  opacityObjText.forEach(element => {
+    let elementPos = element.getBoundingClientRect();
+
+    if (elementPos.y >= position.height / 2) {
+      element.className = "opacity-none";
+    }
+  });
 });
